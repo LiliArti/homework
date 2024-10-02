4d8d61e060644bcd067aac1310008b989eb05570
Lili Stoliarchuk <lil2016st@gmail.com>
2024-10-02 17:21:22 +0200

    main_url commit

--- homework/urls.py
+++ homework/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from myapp.views import main
 
 urlpatterns = [
+    path('', main),
     path('admin/', admin.site.urls),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,2 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
+
+def main(request: HttpRequest) -> HttpResponse:
+     return HttpResponse("основная страница, на которой будет список всех статей.")
